6e40e7e020d66bbaf782b2ec90ca831ad4347013
Elmega123 <yourpalbussiness@gmail.com>
2022-04-17 19:43:25 -0500

    local storage book api

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ function getFetch(){
   const choice = document.querySelector('input').value
   console.log(choice)
   const url = `https://openlibrary.org/isbn/${choice}.json`
-  // `https://api.nasa.gov/planetary/apod?api_key=iJy0Nd4wZZzfW2HFgBkNV4DDUsZ6PxL3RqQRgFfI&date=${choice}`
   fetch(url)
   .then(res => res.json()) // parse response as JSON
   .then(data => {
